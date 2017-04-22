ca9f26219237b0fdb1fe72692a5cd6c91f702cee
johnykapoor <kapoor.johny11@gmail.com>
2017-04-22 16:46:43 +0530

    its third branch second file

---
+++ Registration.java
@@ -0,0 +1,15 @@
+package com.example.administrator.gitdemoone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        String mas =" its   branch 3 second file";
+    }
+}
