330e948a9203aa39b9e6de874a2f988765765f07
johnykapoor <kapoor.johny11@gmail.com>
2017-04-22 16:41:54 +0530

    its second branch second file

---
+++ loginActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.gitdemoone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        String massage= "its login file  for branch two";
+    }
+}
